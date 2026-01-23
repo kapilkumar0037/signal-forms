2ae3f2318bec2cee0a48fe012ea5e26757e06b14
Kapil Kumar <kapilkumar0037@gmail.com>
2026-01-23 21:13:00 +0530

    validation

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,6 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { form, FormField } from '@angular/forms/signals';
+import { form, FormField, required } from '@angular/forms/signals';
 
+
+interface LoginData {
+  email: string;
+  password: string;
+}
 @Component({
   selector: 'app-login',
   imports: [FormField],
@@ -8,9 +13,16 @@ import { form, FormField } from '@angular/forms/signals';
   styleUrl: './login.css',
 })
 export default class Login {
-  loginModel = signal({
+  loginModel = signal<LoginData>({
     email: '',
     password: ''
   })
-  loginForm = form(this.loginModel)
+  loginForm = form(this.loginModel, (schema)=> {
+    required(schema.email, {message: 'Email is required'});
+    required (schema.password, {message: 'Password is required'});
+  })
+
+  // ngOnInit() {
+  //   this.loginForm.email().value.set('kapil@gmail.com')
+  // }
 }
