1a28a7f9cdb470dcfee90eb898952e9c3e7b306a
Kapil Kumar <kapilkumar0037@gmail.com>
2026-01-24 20:27:22 +0530

    validation

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { form, FormField, required } from '@angular/forms/signals';
+import { form, FormField, maxLength, minLength, required } from '@angular/forms/signals';
 
 
 interface LoginData {
@@ -20,6 +20,9 @@ export default class Login {
   loginForm = form(this.loginModel, (schema)=> {
     required(schema.email, {message: 'Email is required'});
     required (schema.password, {message: 'Password is required'});
+    minLength(schema.password, 8, { message: 'Password must be at least 8 characters' })
+    maxLength(schema.password, 25, { message: 'Password is too long' })
+
   })
 
   // ngOnInit() {
