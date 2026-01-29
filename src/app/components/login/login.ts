4644f6d2a1ee4fa93a4f546e8830871986d84397
Kapil Kumar <kapilkumar0037@gmail.com>
2026-01-29 22:05:49 +0530

    tnc checkbox added

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -5,6 +5,7 @@ import { form, FormField, maxLength, minLength, pattern, required, SchemaPathTre
 interface LoginData {
   email: string;
   password: string;
+  acceptTnC: boolean;
 }
 @Component({
   selector: 'app-login',
@@ -15,18 +16,16 @@ interface LoginData {
 export default class Login {
   loginModel = signal<LoginData>({
     email: '',
-    password: ''
+    password: '',
+    acceptTnC: false
   })
   loginForm = form(this.loginModel, (schema) => {
     required(schema.email, { message: 'Email is required' });
     required(schema.password, { message: 'Password is required' });
+    required(schema.acceptTnC, { message: 'You must accept the Terms and Conditions' });
     minLength(schema.password, 8, { message: 'Password must be at least 8 characters' })
     maxLength(schema.password, 25, { message: 'Password is too long' });
     minLength(schema.email, 2, { message: 'Must be at least 2 characters' })
 
   })
-
-  // ngOnInit() {
-  //   this.loginForm.email().value.set('kapil@gmail.com')
-  // }
 }
